7fe3e0fe9d1d722c4ffc861566a32b59aef53466
mirwinli <mirwinli.tech@gmail.com>
2026-04-14 10:56:03 +0300

    env setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mirwinli/coffe_plus
+
+go 1.26.1
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,42 @@
+CREATE SCHEMA coffe_plus;
+CREATE TABLE coffe_plus.users (
+    id              UUID             PRIMARY KEY,
+    version         BIGINT           DEFAULT 1,
+    password_hash   VARCHAR(1000),
+    first_name      VARCHAR(100)     CHECK ( char_length(first_name) BETWEEN 3 AND 100),
+    last_name       VARCHAR(100)     CHECK ( char_length(last_name) BETWEEN 3 AND 100),
+    email           VARCHAR(250) UNIQUE NOT NULL,
+    phone_number    VARCHAR(15)      CHECK (
+        phone_number ~ '^\+[0-9]+$'
+        AND
+        char_length (phone_number) BETWEEN 10 AND 15
+    ),
+
+    is_admin        BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE coffe_plus.products (
+    id           UUID PRIMARY KEY,
+    version      BIGINT DEFAULT 1,
+    name         VARCHAR(100) NOT NULL,
+    description  VARCHAR(1000),
+    price        INTEGER NOT NULL,
+    is_available BOOLEAN NOT NULL,
+    image_url    VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE coffe_plus.orders (
+    id          UUID                  PRIMARY KEY,
+    user_id     UUID         NOT NULL REFERENCES coffe_plus.users(id),
+    status      VARCHAR(150) NOT NULL,
+    total_price DECIMAL       NOT NULL,
+    created_at  TIMESTAMPTZ  NOT NULL
+);
+
+CREATE TABLE coffe_plus.order_items (
+    id              UUID             PRIMARY KEY,
+    order_id        UUID    NOT NULL REFERENCES coffe_plus.orders(id),
+    product_id      UUID    NOT NULL REFERENCES coffe_plus.products(id),
+    quantity        BIGINT  NOT NULL,
+    price_at_time   DECIMAL NOT NULL
+);
